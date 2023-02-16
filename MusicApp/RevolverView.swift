f349a176465ea119fbf697afd0bfd1740d80c74f
Dara adekore <adekore8@gmail.com>
2023-02-16 02:26:56 -0800

    initialization

---
+++ MusicApp/RevolverView.swift
@@ -0,0 +1,29 @@
+//
+//  RevolverView.swift
+//  MusicApp
+//
+//  Created by Dara Adekore on 2023-02-15.
+//
+
+import UIKit
+
+class RevolverView: UIView {
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
---
+++ MusicApp/TrackResponse.swift
@@ -0,0 +1,52 @@
+//
+//  Track.swift
+//  MusicApp
+//
+//  Created by Dara Adekore on 2023-02-15.
+//
+
+import Foundation
+
+// MARK: - TrackResponse
+struct TrackResponse: Codable {
+    let resultCount: Int
+    let results: [Track]
+}
+
+// MARK: - Result
+struct Track: Codable {
+    let wrapperType, kind: String?
+    let artistID: Int?
+    let collectionID: Int?
+    let trackID: Int?
+    let artistName: String?
+    let collectionName: String?
+    let trackName: String?
+    let collectionCensoredName: String?
+    let trackCensoredName: String?
+    let artistViewURL: String?
+    let collectionViewURL: String?
+    let trackViewURL: String?
+    let previewURL: String?
+    let artworkUrl30, artworkUrl60, artworkUrl100: String?
+    let collectionPrice, trackPrice: Double?
+    let releaseDate: String?// could be of type Date
+    let collectionExplicitness, trackExplicitness: String?
+    let discCount, discNumber, trackCount, trackNumber: Int?
+    let trackTimeMillis: Int?
+    let country, currency, primaryGenreName: String?
+    let isStreamable: Bool?
+    
+    enum CodingKeys: String, CodingKey {
+        case wrapperType, kind
+        case artistID = "artistId"
+        case collectionID = "collectionId"
+        case trackID = "trackId"
+        case artistName, collectionName, trackName, collectionCensoredName, trackCensoredName
+        case artistViewURL = "artistViewUrl"
+        case collectionViewURL = "collectionViewUrl"
+        case trackViewURL = "trackViewUrl"
+        case previewURL = "previewUrl"
+        case artworkUrl30, artworkUrl60, artworkUrl100, collectionPrice, trackPrice, releaseDate, collectionExplicitness, trackExplicitness, discCount, discNumber, trackCount, trackNumber, trackTimeMillis, country, currency, primaryGenreName, isStreamable
+    }
+}
